6a9c50e283b9ada295afff25a02088429625c0af
Dylan Hassinger <dylanhassinger@gmail.com>
2015-12-26 18:44:55 -0600

    added test options override tests

--- test/101-test-basics.js
+++ test/101-test-basics.js
@@ -39,6 +39,55 @@
 			}
 		]
 	);
-
 	
+// options override - setup
 
+	var foo = 0;
+	var bar = false;
+
+	var options_override_tests = [
+		{
+			label: "Use inherited assert function"
+		},
+		{
+			label: "Override inherited assert function",
+			assert: function() {
+			
+				test.assert(bar === false);
+				bar = true;
+					
+			}
+		}		
+	];
+	
+	var options_override_options = {
+		assert: function() {
+			
+			test.assert(foo === 0);
+			foo++;			
+			
+		}
+	};
+
+// options override - tests
+
+	test.suite("102.2 Options Override", options_override_tests, options_override_options);
+		
+	test.suite("Options Override (cont'd- checking)", [
+		{
+			label: "Checking foo",
+			assert: function() {
+				
+				test.assert(foo === 1);
+				
+			}
+		},
+		{
+			label: "Checking bar",
+			assert: function() {
+				
+				test.assert(bar === true);
+		
+			}		
+		}		
+	]);	
